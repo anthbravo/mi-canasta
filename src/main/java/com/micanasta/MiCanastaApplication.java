24060dc980f7b38548a1d9768d452fcef6fa116c
Anthony Joaquin Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-04-13 16:16:39 -0500

    Se empieza a contruir la estructura del proyecto java + angular

---
+++ src/main/java/com/micanasta/MiCanastaApplication.java
@@ -0,0 +1,13 @@
+package com.micanasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCanastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCanastaApplication.class, args);
+	}
+
+}

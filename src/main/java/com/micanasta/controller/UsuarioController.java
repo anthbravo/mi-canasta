9a65479de458ed4f691113cf1e1e529c50647eb9
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-05-20 21:59:20 -0500

    Se agrega Exceptions
    Se modificó en el Service, Controller y se creó una nueva exception class.

--- src/main/java/com/micanasta/controller/UsuarioController.java
+++ src/main/java/com/micanasta/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.micanasta.controller;
 
 import com.micanasta.dto.UsuarioAccesoDto;
 import com.micanasta.dto.UsuarioLoginDto;
+import com.micanasta.exception.UserLoginIncorrectException;
+import com.micanasta.exception.UserLoginNotFoundException;
 import com.micanasta.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +18,14 @@ public class UsuarioController {
 
     @PostMapping("usuarios")
     public ResponseEntity<?> ValidarIngreso(@RequestBody UsuarioLoginDto usuarioLoginDto) {
+        try{
         UsuarioAccesoDto usuarioAccesoDto = usuarioService.ValidateLogin(usuarioLoginDto.dni, usuarioLoginDto.contrasena);
-        if (usuarioAccesoDto.dni == "NotFound") {
-            return ResponseEntity.status(401).body(usuarioAccesoDto);
-        }
-        if (usuarioAccesoDto.dni == "NotExist") {
-            return ResponseEntity.status(404).body(usuarioAccesoDto);
-        } else {
-            return ResponseEntity.ok().body(usuarioAccesoDto);
+
+            return ResponseEntity.status(201).body(usuarioAccesoDto);
+        }catch (UserLoginNotFoundException userLoginNotFoundException){
+            return ResponseEntity.status(400).body(userLoginNotFoundException.exceptionDto);
+        }catch (UserLoginIncorrectException userLoginIncorrectException){
+            return ResponseEntity.status(400).body(userLoginIncorrectException.exceptionDto);
         }
     }
 

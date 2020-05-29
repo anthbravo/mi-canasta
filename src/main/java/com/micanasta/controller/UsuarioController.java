650a5fd6c7eb83dd994fdd278972c99f354db07f
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-05-29 01:24:22 -0500

    Update UsuarioController.java

--- src/main/java/com/micanasta/controller/UsuarioController.java
+++ src/main/java/com/micanasta/controller/UsuarioController.java
@@ -19,9 +19,7 @@ import com.micanasta.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsuarioController {

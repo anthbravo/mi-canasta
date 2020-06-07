f0855041b2dbe05c7ec304903372751f5037324f
JYellow363 <jyellow363@gmail.com>
2020-06-07 04:27:09 -0500

    fix ( { )

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -40,6 +40,7 @@ const routes: Routes = [
         loadChildren: ()=>
           import("./modules/home-solicitud/home-solicitud.module").then((m)=>m.HomeSolicitudModule)
       },
+      {
         path: 'home/request-sent',
         loadChildren: ()=>
           import("./modules/requests-sent/requests-sent.module").then((m)=>m.RequestsSentModule)

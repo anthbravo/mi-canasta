47c345b10109c3e0c35de3f9052fb492ae6e7f4d
JYellow363 <jyellow363@gmail.com>
2020-06-08 10:37:28 -0500

    fix hu22

--- src/main/java/com/micanasta/service/CompraService.java
+++ src/main/java/com/micanasta/service/CompraService.java
@@ -3,7 +3,13 @@ package com.micanasta.service;
 
 import com.micanasta.dto.CompraCreateDto;
 import com.micanasta.dto.CompraDto;
+import com.micanasta.dto.CompraUpdateDto;
+
+import java.util.Date;
+import java.util.List;
 
 public interface CompraService {
     CompraCreateDto create(CompraCreateDto compraDto);
+    List<CompraDto> getCompras(long idFamilia, String dni, Date fechaInicio, Date fechaFinal) throws Exception;
+    CompraUpdateDto update(CompraUpdateDto historialDto) throws Exception;
 }
--- src/main/java/com/micanasta/service/impl/CompraServiceImpl.java
+++ src/main/java/com/micanasta/service/impl/CompraServiceImpl.java
@@ -1,12 +1,19 @@
 package com.micanasta.service.impl;
 
 import com.micanasta.dto.CompraCreateDto;
+import com.micanasta.dto.CompraDto;
+import com.micanasta.dto.CompraUpdateDto;
 import com.micanasta.dto.converter.CompraDtoConverter;
 import com.micanasta.model.Compra;
 import com.micanasta.repository.CompraRepository;
 import com.micanasta.service.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CompraServiceImpl implements CompraService {
@@ -24,4 +31,47 @@ public class CompraServiceImpl implements CompraService {
         compraRepository.save(compra);
         return compraDto;
     }
+
+    @Override
+    public List<CompraDto> getCompras(long idFamilia, String dni, Date fechaInicio, Date fechaFinal) throws Exception {
+        List<CompraDto> comprasDto;
+        List<Compra> comprasAux = new ArrayList<>();
+
+        for (Compra compra : compraRepository.getByCompraIdentityFamiliaIdAndDni(idFamilia, dni)) {
+            if (compra.getFechaCompra().after(fechaInicio) && compra.getFechaCompra().before(fechaFinal)) {
+                comprasAux.add(compra);
+            }
+        }
+
+        comprasDto = comprasAux.stream().map(x -> {
+            CompraDto compraDto = new CompraDto();
+            compraDto.setDni(x.getDni());
+            compraDto.setCantidad(x.getCantidad());
+            compraDto.setFechaCompra(x.getFechaCompra());
+            compraDto.setFamiliaId(x.getCompraIdentity().getFamilia().getId());
+            compraDto.setProductoId(x.getCompraIdentity().getProducto().getId());
+            compraDto.setTiendaId(x.getCompraIdentity().getTienda().getId());
+            compraDto.setConfirmacion(x.isConfirmacion());
+            return compraDto;
+        }).collect(Collectors.toList());
+        if(comprasDto.size()==0) throw new Exception();
+        return comprasDto;
+    }
+
+    @Override
+    public CompraUpdateDto update(CompraUpdateDto compraDto) throws Exception {
+        Compra compra =
+                compraRepository.getByCompraIdentityFamiliaIdAndCompraIdentityProductoIdAndCompraIdentityTiendaId(compraDto.getFamiliaId(), compraDto.getProductoId(),
+                        compraDto.getTiendaId());
+        if(compra!=null){
+           compra.setDni(compraDto.getDni());
+           compra.setFechaCompra(compraDto.getFechaCompra());
+           compra.setConfirmacion(compraDto.isConfirmacion());
+           compra.setCantidad(compraDto.getCantidad());
+           compraRepository.save(compra);
+        }
+        else throw new Exception();
+        return compraDto;
+    }
 }
+

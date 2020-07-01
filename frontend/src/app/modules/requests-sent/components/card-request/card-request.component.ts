24da9fdb79f1cc87859b0f8d2288c1d91ed7d85a
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-07-01 18:31:30 -0500

    historias completa

--- frontend/src/app/modules/requests-sent/components/card-request/card-request.component.ts
+++ frontend/src/app/modules/requests-sent/components/card-request/card-request.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SolicitudService } from '../../../../core/service/solicitud.service';
 
 @Component({
-  selector: 'app-card-request',
-  templateUrl: './card-request.component.html',
-  styleUrls: ['./card-request.component.scss']
+    selector: 'app-card-request',
+    templateUrl: './card-request.component.html',
+    styleUrls: ['./card-request.component.scss'],
 })
 export class CardRequestComponent implements OnInit {
-  @Input()
-  request:any;
+    @Input()
+    request: any;
 
-  constructor() { }
+    @Output()
+    eventRefresh = new EventEmitter<any>();
 
-  ngOnInit(): void {
-  }
+    constructor(private solicitudService: SolicitudService) {}
 
+    ngOnInit(): void {}
+    async cancelarSolicitud() {
+        try {
+            const value = await this.solicitudService.denegarSolicitud(
+                this.request.dni
+            );
+            console.log(value);
+            this.eventRefresh.emit();
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
